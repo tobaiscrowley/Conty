3d39be9fed011dbc73201a1cedbcaabed7f17a6f
tobaiscrowley <tobaiscrowley@gmail.com>
2025-05-20 14:32:07 +0000

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-# shellcheck shell=bash disable=2034
+2# shellcheck shell=bash disable=2034
 
 # Packages to install
 # You can add packages that you want and remove packages that you don't need
@@ -41,13 +41,24 @@ PACKAGES=(
 	# development
 	base-devel git meson mingw-w64-gcc cmake
 	# gaming
+	lutris python-protobuf steam steam-native-runtime steamtinkerlaunch
+	minigalaxy gamehub legendary prismlauncher bottles playonlinux obs-studio
+	retroarch retroarch-assets-ozone libretro-beetle-psx-hw sunshine
+	libretro-blastem libretro-bsnes libretro-dolphin duckstation
+	libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia
+	libretro-parallel-n64 libretro-picodrive libretro-ppsspp
+	libretro-retrodream libretro-yabause pcsx2-avx-git
 	# extra
+	nano ttf-dejavu ttf-liberation firefox mpv geany pcmanfm
+	htop qbittorrent speedcrunch gpicview file-roller openbox lxterminal
+	yt-dlp minizip nautilus genymotion jre17-openjdk gnome-themes-extra
+ 	# fork extras
  	obs-studio qbittorrent signal-desktop
  
 )
 
 # If you want to install AUR packages, specify them in this variable
-AUR_PACKAGES=(localsend-bin)
+AUR_PACKAGES=(localsend-bin faugus-launcher-git)
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
