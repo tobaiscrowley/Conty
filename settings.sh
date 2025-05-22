124e9d71a88233c3afe8d9604828a21b5339b3a9
tobaiscrowley <tobaiscrowley@gmail.com>
2025-05-22 04:49:01 +0000

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -27,7 +27,7 @@ PACKAGES=(
 )
 
 # If you want to install AUR packages, specify them in this variable
-AUR_PACKAGES=(localsend-bin)
+AUR_PACKAGES=(localsend-bin librewolf-bin)
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
