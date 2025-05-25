e8bcd6d2ccc851d0afa4ae9750d8b256251a9e7f
tobaiscrowley <tobaiscrowley@gmail.com>
2025-05-25 00:34:42 +0000

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -55,7 +55,7 @@ PACKAGES=(
 )
 
 # If you want to install AUR packages, specify them in this variable
-AUR_PACKAGES=(faugus-launcher-git localsend-bin librewolf-bin librewolf-extension-tridactyl-bin firefox-extension-singlefile-bin)
+AUR_PACKAGES=(faugus-launcher-git localsend-bin librewolf-bin)
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
